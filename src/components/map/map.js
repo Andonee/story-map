9725a3f63243f749c2a7829e46b0b59cc8b3889f
Mateusz <mateusz.poprawa@gmail.com>
2020-04-04 11:25:51 +0200

    create MapContaner component

---
+++ src/components/map/map.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { Map, TileLayer } from 'react-leaflet'
+import styles from './map.module.css'
+
+class MapContainer extends React.Component {
+  state = {
+    lat: 0,
+    lng: 0,
+    zoom: 2
+  }
+
+  render(){
+    const position = [this.state.lat, this.state.lng]
+    return (
+      <Map center={position} zoom={this.state.zoom} className={styles.map_container}>
+        <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+              />
+      </Map>
+
+    )
+  }
+  
+}
+
+
+  export default MapContainer
