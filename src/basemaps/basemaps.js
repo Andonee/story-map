15881d450810f3ebd1909d43393751b7f4f8ceb3
Mateusz <mateusz.poprawa@gmail.com>
2020-04-04 12:46:25 +0200

    add basemaps control

---
+++ src/basemaps/basemaps.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import { LayersControl, TileLayer} from 'react-leaflet'
+
+import styles from './basemaps.module.css'
+
+const Basemaps = () => {
+  return (
+    <LayersControl position="topright">
+      <LayersControl.BaseLayer name="OpenStreetMap.BlackAndWhite" checked="true">
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+        />
+      </LayersControl.BaseLayer>
+      <LayersControl.BaseLayer name="OpenStreetMap.Mapnik">
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </LayersControl.BaseLayer>
+      <LayersControl.BaseLayer name="Stadia.AlidadeSmoothDark">
+        <TileLayer
+          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        />
+      </LayersControl.BaseLayer>
+      <LayersControl.BaseLayer name="OpenTopoMap">
+        <TileLayer
+          attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+          url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+        />
+      </LayersControl.BaseLayer>
+      <LayersControl.BaseLayer name="Esri.WorldImagery">
+        <TileLayer
+          attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+        />
+      </LayersControl.BaseLayer>
+    </LayersControl>
+  )
+}
+
+export default Basemaps
--- src/components/map/map.js
+++ src/components/map/map.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { Map, TileLayer } from 'react-leaflet'
 import styles from './map.module.css'
+import styless from '../../basemaps/basemaps'
+import Basemaps from '../../basemaps/basemaps'
 
 class MapContainer extends React.Component {
   state = {
@@ -14,10 +16,11 @@ class MapContainer extends React.Component {
     const position = [this.state.lat, this.state.lng]
     return (
       <Map center={position} zoom={this.state.zoom} className={styles.map_container}>
-        <TileLayer
+        {/* <TileLayer
                 attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-              />
+              /> */}
+        <Basemaps color= "red"/>
       </Map>
 
     )
